696a0650d4bc7590daa9441ba13904d6092f602c
karulku <160414535+karulku@users.noreply.github.com>
2025-12-16 20:20:06 +0800

    Update AT24C02.h

--- HNU-单片机代码/lab4/mybsp/AT24C02.h
+++ HNU-单片机代码/lab4/mybsp/AT24C02.h
@@ -2,11 +2,15 @@
 #define __AT24C02_h__
 
 /*
-0,1 - 保存点赞数 0保存高8bit 1保存低8bit
+main中调用Init即可。
+AT24C02有256字节的非易失存储，对应地址0x00-0xff
+  rAT：读取某个地址的数据
+  wAT：向某个地址写数据
 */
 
 extern void ATInit();
 extern unsigned char rAT(unsigned char addr);
 extern void wAT(unsigned char addr, unsigned char val);
+
 
 #endif
