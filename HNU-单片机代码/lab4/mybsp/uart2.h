e1003387df5decf28b561eedea9994b36f82a6c9
karulku <160414535+karulku@users.noreply.github.com>
2025-12-16 22:38:34 +0800

    Update uart2.h

--- HNU-单片机代码/lab4/mybsp/uart2.h
+++ HNU-单片机代码/lab4/mybsp/uart2.h
@@ -5,7 +5,16 @@
 和串口1类似。
 uart2没有做包头检测，只需要传入接收数据的缓冲区
 uart2使用定时器2做波特率产生器，不同于uart1使用定时器1，所以两个串口的波特率可以不同；
-这里的波特率采用了lab2学长的计算方法，和uart1的查表不同，支持的波特率没有进行测试，但应该比uart1更多
+波特率只有9600测试正常，其他没有测试（因为我做实验用的是9600）
+支持的波特率：
+  1200
+  2400
+  4800
+  9600
+  19200
+  38400
+  57600
+  115200
 */
 
 extern void uart2Init(unsigned long baudrate);
@@ -14,3 +23,4 @@ extern void setUart2Buf(unsigned char* buf, unsigned char buf_num);
 
 
 #endif
+
