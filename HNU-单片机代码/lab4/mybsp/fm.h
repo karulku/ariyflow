71166654292f3ed5768424c26ab2299f8195e7f2
karulku <160414535+karulku@users.noreply.github.com>
2025-12-16 20:26:29 +0800

    Update fm.h

--- HNU-单片机代码/lab4/mybsp/fm.h
+++ HNU-单片机代码/lab4/mybsp/fm.h
@@ -1,8 +1,15 @@
 #ifndef __FM_H__
 #define __FM_H__
 
+/*
+main中调用Init函数
+打开收音机或者修改音量，频率时，调用update_params函数
+关闭时调用stopFM函数
+*/
+
 extern void fmInit();
 extern void update_params(unsigned int fre, unsigned char vol);
 extern void stopFM();
+
 
 #endif
